1ba3093f95d1d7882b7e6d3fa23ac92b306f3392
7TIN <prasadgujarathi619@gmail.com>
2025-07-29 21:23:16 +0530

    feat: added the global error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,11 @@
 "use server";
 
 import { z } from "zod";
-import postgres from 'postgres';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
@@ -26,37 +26,44 @@ export async function createInvoice(formData: FormData) {
     status: formData.get("status"),
   });
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
-
-  await sql`
+  const date = new Date().toISOString().split("T")[0];
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+  } catch (error) {
+    console.log(error);
+  }
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string) {  
+    // throw new Error('Failed to Delete Invoice');
+
   await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  revalidatePath("/dashboard/invoices");
 }
- 
+
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
   });
- 
+
   const amountInCents = amount * 100;
- 
-  await sql`
+
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+  } catch (error) {
+    console.log(error);
+  }
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
-
